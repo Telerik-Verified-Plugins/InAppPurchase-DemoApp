e878a2a4c3d906275f47843fab25b3a6ba62aee1
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-01-09 10:39:02 +0000

    demo app for the InAppPurchase plugin

---
+++ InAppPurchase/scripts/demo.js
@@ -0,0 +1,145 @@
+(function (global) {
+    var DemoViewModel,
+        app = global.app = global.app || {};
+
+    DemoViewModel = kendo.data.ObservableObject.extend({
+
+        initStore: function () {
+            if (!this.checkSimulator()) {
+              // Enable maximum logging level
+              store.verbosity = store.DEBUG;
+
+              // Enable remote receipt validation
+              // TODO test without
+              store.validator = "https://api.fovea.cc:1982/check-purchase";
+
+              // Inform the store of your products
+              store.register({
+                               id:    'com.telerik.demoproduct.consumable1',
+                               alias: '100 coins',
+                               type:   store.CONSUMABLE
+                             });
+              
+              store.register({
+                               id:    'com.telerik.demoproduct.nonconsumable1',
+                               alias: 'Full version',
+                               type:   store.NON_CONSUMABLE
+                             });
+              
+              store.register({
+                               id:    'com.telerik.demoproduct.subscription1',
+                               alias: 'subscription1',
+                               type:  store.PAID_SUBSCRIPTION
+                             });
+
+              // When any product gets updated, refresh the HTML
+              store.when("product").updated(function (p) {
+                var container = document.getElementById('productContainer');
+                var elId = p.id.split(".")[3];
+                var el = document.getElementById(elId);
+                if (!el) {
+                  container.innerHTML += '<div id="'+elId+'"></div>';
+                  el = document.getElementById(elId);
+                }
+                
+                if (!p.loaded) {
+                  el.innerHTML += '<h3>...</h3>';
+                } else if (!p.valid) {
+                  el.innerHTML += '<h3>' + p.alias + ' Invalid</h3>';
+                } else if (p.valid) {
+                  var html = "<h3>" + p.title + "</h3>" + "<p>" + p.description + "</p>";
+                  if (p.canPurchase) {
+                    html += "<button class='button' onclick='store.order(\"" + p.id + "\")'>" + p.price + "</button>";
+                  }
+                  el.innerHTML = html;
+                }
+              });
+
+              // handle subscription events
+              store.when("subscription1").approved(function(p) {
+                                         alert("verify subscription");
+                                         p.verify();
+                                       });
+
+              store.when("subscription1").verified(function(p) {
+                                                     alert("subscription verified");
+                                                     p.finish();
+                                                   });
+
+              store.when("subscription1").unverified(function(p) {
+                                                       alert("subscription unverified");
+                                                     });
+
+              store.when("subscription1").updated(function(p) {
+                                                    if (p.owned) {
+                                                      console.log('You have a subscription');
+                                                      //document.getElementById('subscriber-info').innerHTML = 'You are a lucky subscriber!';
+                                                    }
+                                                    else {
+                                                      console.log('You don\'t have a subscription');
+                                                      //document.getElementById('subscriber-info').innerHTML = 'You are not subscribed';
+                                                    }
+                                                  });
+              // Deal with errors
+              store.error(function(error) {
+                            alert('ERROR ' + error.code + ': ' + error.message);
+                          });
+  
+              // When purchase of 100 coins is approved, show an alert
+              store.when("100 coins").approved(function (order) {
+                                                  alert("You got an additional 100 coins!");
+                                                  order.finish();
+                                                });
+
+              // When purchase of the full version is approved, show an alert and finish the transaction.
+              store.when("Full version").approved(function (order) {
+                                                    alert('You just unlocked the FULL VERSION!');
+                                                    order.finish();
+                                                  });
+  
+              // The play button can only be accessed when the user owns the full version.
+              store.when("Full version").updated(function (product) {
+                                                   alert("The full version updated to " + product.owned ? "owned" : "not owned");
+                                                 });
+
+              // When the store is ready all products are loaded and in their "final" state.
+              // Note that the "ready" function will be called immediately if the store is already ready.
+              // When the store is ready, activate the "refresh" button;
+              store.ready(function() {
+                            alert("The store is ready"); // TODO change to console.log
+                // TODO implement, but could do without, see note below
+                /*
+                            var el = document.getElementById('refresh-button');
+                            if (el) {
+                              el.style.display = 'block';
+                              el.onclick = function(ev) {
+                                store.refresh();
+                              };
+                            }
+                            */
+                          });
+
+              // Refresh the store.
+              // This will contact the server to check all registered products validity and ownership status.
+              // It's fine to do this only at application startup, as it could be pretty expensive.
+              store.refresh();
+            }
+        },
+
+        checkSimulator: function() {
+            if (window.navigator.simulator === true) {
+                alert('This plugin is not available in the simulator.');
+                return true;
+            } else if (window.store === undefined) {
+                alert('Plugin not found. Maybe you are running in AppBuilder Companion app which currently does not support this plugin.');
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+
+    app.demoService = {
+        viewModel: new DemoViewModel()
+    };
+})(window);
